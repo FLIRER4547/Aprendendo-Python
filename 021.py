3e3bf333632305e3f8bbf8a7d4ee716ae9a16e9d
FLIRER4547 <fernandomarianosilva613@gmail.com>
2025-07-26 16:56:37 -0300

    ...

--- 021.py
+++ 021.py
@@ -6,7 +6,3 @@ pygame.mixer.music.load(a)
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     pygame.time.Clock().tick(10)
-<<<<<<< HEAD
-
-=======
->>>>>>> 0af5b84 (adicionar todos os arquivos)
